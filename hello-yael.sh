fa4dcb51a21d642e3ff52f830bd2e6b8ed080247
Yael <yaelsprikut@users.noreply.github.com>
2022-08-25 21:31:27 -0400

    Create hello-yael.sh

---
+++ hello-yael.sh
@@ -0,0 +1,33 @@
+Linux chia 5.10.17-v7l+ #1403 SMP Mon Feb 22 11:33:35 GMT 2021 armv7l
+Welcome to Chia Pi!
+
+		   .~~.   .~~.
+		  '. \ ' ' / .'
+		   .~ .~~~..~.
+		  : .~.'~'.~. :
+		 ~ (   ) (   ) ~
+		( : '~'.~.'~' : )
+		 ~ .~ (   ) ~. ~
+		  (  : '~' :  )
+		   '~ .~~~. ~'
+		       '~'
+
+@@@  @@@  @@@@@@@@  @@@ @@@    @@@ @@@   @@@@@@   @@@@@@@@  @@@
+@@@  @@@  @@@@@@@@  @@@ @@@    @@@ @@@  @@@@@@@@  @@@@@@@@  @@@
+@@!  @@@  @@!       @@! !@@    @@! !@@  @@!  @@@  @@!       @@!
+!@!  @!@  !@!       !@! @!!    !@! @!!  !@!  @!@  !@!       !@!
+@!@!@!@!  @!!!:!     !@!@!      !@!@!   @!@!@!@!  @!!!:!    @!!
+!!!@!!!!  !!!!!:      @!!!       @!!!   !!!@!!!!  !!!!!:    !!!
+!!:  !!!  !!:         !!:        !!:    !!:  !!!  !!:       !!:
+:!:  !:!  :!:         :!:        :!:    :!:  !:!  :!:        :!:
+::   :::   :: ::::     ::         ::    ::   :::   :: ::::   :: ::::
+ :   : :  : :: ::      :          :      :   : :  : :: ::   : :: : :
+
+The programs included with the Debian GNU/Linux system are free software;
+the exact distribution terms for each program are described in the
+individual files in /usr/share/doc/*/copyright.
+
+Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent
+permitted by applicable law.
+Last login: Sun Aug 14 17:13:22 2022
+pi@chia:~ $
