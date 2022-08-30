63e7c28304d58405345f4d1381cbdb81fa2cd973
Yael Sprikut <yaelsprikut@yaelmacmini.local>
2022-08-30 18:05:25 -0400

    add musicbox

---
+++ musicbox.sh
@@ -0,0 +1,53 @@
+Linux musicbox 5.10.17-v7+ #1403 SMP Mon Feb 22 11:29:51 GMT 2021 armv7l
+
+             ;I;
+             +It.
+             +II+
+             +III;
+             +IIII;
+             iIIIII;
+             titIIII;
+             I;.iIIII;
+            .I;  =IIII;
+            .I;   ;IIII:
+            .I;    :tIIt
+            .I;     .tII=
+            .I;      :III
+            :I;       +II;
+            :I;       .II=
+            :I:        iIi
+            :I.        ;Ii
+            ;I.        =I;
+            ;I.        tt
+            +I        ;I;
+            +I       ;I;
+            tI      ;I;
+     :;itIIIII     +t:
+   ;tIIIIIIIII   :t;
+ .tIIIIIIIIIIi  ;i.
+.tIIIIIIIIIII;.;:
+;IIIIIIIIIII;
+;IIIIIIIIIt;
+ ;tIIIIt+;
+   ....
+
+
+
+
+
+
+███╗   ███╗██╗   ██╗███████╗██╗ ██████╗██████╗  ██████╗ ██╗  ██╗    ██████╗ ██╗
+████╗ ████║██║   ██║██╔════╝██║██╔════╝██╔══██╗██╔═══██╗╚██╗██╔╝    ██╔══██╗██║
+██╔████╔██║██║   ██║███████╗██║██║     ██████╔╝██║   ██║ ╚███╔╝     ██████╔╝██║
+██║╚██╔╝██║██║   ██║╚════██║██║██║     ██╔══██╗██║   ██║ ██╔██╗     ██╔═══╝ ██║
+██║ ╚═╝ ██║╚██████╔╝███████║██║╚██████╗██████╔╝╚██████╔╝██╔╝ ██╗    ██║     ██║
+╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝ ╚═════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝    ╚═╝     ╚═╝
+
+
+
+The programs included with the Debian GNU/Linux system are free software;
+the exact distribution terms for each program are described in the
+individual files in /usr/share/doc/*/copyright.
+
+Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent
+permitted by applicable law.
